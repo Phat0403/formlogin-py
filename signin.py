ac73b18b127f5a7bfa3935f459ca5d41fc7451c8
Phat0403 <dangtanphat9a1@gmail.com>
2022-12-16 23:03:06 +0700

    COn ngua tat

--- signin.py
+++ signin.py
@@ -1,3 +1,4 @@
+import pygame, sys
 import tkinter
 import ast
 from tkinter import *
@@ -33,7 +34,56 @@ def signin():
 
 
     if username in r.keys() and password==r[username]:
-        print(username+' '+'sign in')
+        class Player(pygame.sprite.Sprite):
+            def __init__(self,pos_x, pox_y) :
+                super().__init__()
+                self.is_animating=False
+                self.sprites=[]
+                self.sprites.append(pygame.image.load('D:\python\python dang nhap\Main login\ice-horse-run-01.png'))
+                self.sprites.append(pygame.image.load('D:\python\python dang nhap\Main login\ice-horse-run-02.png'))
+                self.sprites.append(pygame.image.load('D:\python\python dang nhap\Main login\ice-horse-run-03.png'))
+                self.sprites.append(pygame.image.load('D:\python\python dang nhap\Main login\ice-horse-run-04.png'))
+                self.sprites.append(pygame.image.load('D:\python\python dang nhap\Main login\ice-horse-run-05.png'))
+                self.sprites.append(pygame.image.load('D:\python\python dang nhap\Main login\ice-horse-run-06.png'))
+                self.sprites.append(pygame.image.load('D:\python\python dang nhap\Main login\ice-horse-run-07.png'))
+
+                self.current_sprite=0
+                self.image=self.sprites[self.current_sprite]
+                self.rect=self.image.get_rect(topleft=(pos_x,pox_y))
+            def atack(self):
+                self.is_animating=True
+            def update(self,speed):
+                if self.is_animating==True:
+                    self.current_sprite+=speed
+                    if self.current_sprite>= len(self.sprites):
+                        self.current_sprite=0
+
+                self.image=self.sprites[int(self.current_sprite)]
+
+
+    moving_sprite=pygame.sprite.Group()
+    player=Player(425,200)
+    moving_sprite.add(player)
+
+    pygame.init()
+
+    screen=pygame.display.set_mode((925,500))
+    pygame.display.set_caption('Ngua')
+    clock=pygame.time.Clock()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type==pygame.KEYDOWN:
+                player.atack()
+        screen.fill(('gray'))
+        moving_sprite.draw(screen)
+        moving_sprite.update(0.2)
+        pygame.display.flip()
+        clock.tick(60)
+
     else:
         messagebox.showerror('Invalid','Invalid username or password')
 def signup_command():
